e00093abb0614bd34ef5a143c5ccf8f3f3913608
Aamir Laasi <aamirlaasi@Aamirs-MacBook-Air.local>
2017-09-25 23:22:04 -0500

    finished hw

---
+++ app/data/friends.js
@@ -0,0 +1,105 @@
+// All of the data for the application is stored
+// in this file as an array of objects.
+// ----------------------------------------
+
+var friends = [
+	{
+		"name":"Friend1",
+		"image":"https://wallpaperbrowse.com/media/images/cool-pictures-24.jpg",
+		"points": [
+		"4",
+		"4",
+		"4",
+		"4",
+		"4",
+		"4",
+		"4",
+		"4",
+		"4",
+		"4"
+		]
+	},
+	{
+		"name":"Friend2",
+		"image":"https://wallpaperbrowse.com/media/images/cool-pictures-19.jpg",
+		"points": [
+		"2",
+		"3",
+		"4",
+		"5",
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"1"
+		]
+	},
+	{
+		"name":"Friend3",
+		"image":"https://wallpaperbrowse.com/media/images/download_8iXWpwE.jpg",
+		"points": [
+		"3",
+		"4",
+		"5",
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"1",
+		"2"
+		]
+	},
+	{
+		"name":"Friend4",
+		"image":"https://wallpaperbrowse.com/media/images/cool-pic-4.jpg",
+		"points": [
+		"4",
+		"3",
+		"3",
+		"2",
+		"1",
+		"1",
+		"4",
+		"5",
+		"4",
+		"3"
+		]
+	},
+	{
+		"name":"Friend5",
+		"image":"https://wallpaperbrowse.com/media/images/Cool-And-Beautiful-Wallpaper.jpg",
+		"points": [
+		"1",
+		"1",
+		"1",
+		"2",
+		"2",
+		"2",
+		"3",
+		"4",
+		"4",
+		"4"
+		]
+	},
+	{
+		"name":"Friend6",
+		"image":"https://wallpaperbrowse.com/media/images/Cool-Wallpaper-206.jpg",
+		"points": [
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5"
+		]
+	}
+];
+
+// Export the array so it is accessible globally
+module.exports = friends;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+// Require the dependencies
+
+var path = require('path');
+
+// Create and export functions required for the
+// html routes
+
+module.exports = function(app) {
+	// A GET route to /survey that displays the
+	// survey page
+	app.get('/survey', function(req,res){
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	// A default, catch-all route that leads to home.html which 
+	// displays the home page.
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+}
